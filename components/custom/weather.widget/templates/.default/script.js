9d75096ee1f9b61841666a3d706e1c09ea9250b6
Yura <andronat077@gmail.com>
2025-04-23 03:41:00 +0600

    Delete components/custom/weather.widget directory

--- components/custom/weather.widget/templates/.default/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var pulse = document.getElementById('pulse_open_btn');
-    var widget = document.querySelector('.weather-widget');
-    if (pulse && widget) {
-      pulse.insertAdjacentElement('afterend', widget);
-    }
-  });  
